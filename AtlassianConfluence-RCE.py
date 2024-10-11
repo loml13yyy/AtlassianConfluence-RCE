d53fb64cfd85b02c6572fedca05cbc0d1b05f7b4
loml13yyy <yby20923@gmail.com>
2024-10-11 16:40:27 +0800

    Update AtlassianConfluence-RCE.py

--- AtlassianConfluence-RCE.py
+++ AtlassianConfluence-RCE.py
@@ -46,7 +46,7 @@ def startwith():
 if __name__ == '__main__':
     startwith()
     parser = argparse.ArgumentParser(
-        description="This is an AtlassianConfluence-RCE detection exploitation tool")
+        description="This is an AtlassianConfluence-RCE detection tool")
 
     # 添加命令行参数 处理这些参数
     parser.add_argument("-u", "--url", help="Specify the target URL for the attack")
